a0e0a6c1ffcfa40fe6f69811a9c235e778ecd184
DylanSchmidt-Cunningham <da_cunningham@laurentian.ca>
2018-02-27 19:12:09 -0500

    added typescript and createjs support

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -1,9 +1,10 @@
 // IIFE -- Immediately Invoked Function Expression
-(function () {
+(function(){
     //these should also be enclosed, but var's poorly behaved
     //var likes to hoist up everything to the top, make it GLOBAL
     //so we're using let instead so variables stay in-scope
     //and const, too
+
     //js types
     //number
     //string
@@ -12,9 +13,11 @@
     //any (loose typing - NEVER use)
     //and some others
     //js does type inference, it's all implicit
+
     /* STYLE GUIDE
      * we're using _varname to designate a constant
      */
+
     /* wouldn't it be nice if we could specify return types, etc. in
      * javascript?
      * that's where either Babel or Typescript come in
@@ -22,30 +25,41 @@
      * problem: browsers don't understand Typescript
      * have to do transpile
      */
-    var canvas = document.getElementById("canvas");
-    var stage;
-    var helloLabel;
-    function Init() {
+    let canvas:HTMLElement = document.getElementById("canvas");
+    let stage:createjs.Stage;
+    let helloLabel:createjs.Text;
+
+    function Init():void {
         console.log("Initialization Started...");
+
         Start();
     }
-    function Start() {
+
+    function Start():void {
         console.log("Starting Application...");
+
         stage = new createjs.Stage(canvas);
         createjs.Ticker.framerate = 60; // 60 FPS
         createjs.Ticker.on("tick", Update);
         Main();
     }
-    function Update() {
+
+    function Update():void {
         helloLabel.rotation -= 5;
         stage.update(); // redraws the stage
     }
-    function Main() {
+
+    function Main():void {
         console.log("Game Started...");
-        helloLabel = new createjs.Text("Hello World!", "40px Consolas", "#000000");
+
+        helloLabel = new createjs.Text("Hello World!", "40px Consolas",
+         "#000000");
         helloLabel.x = 100;
         helloLabel.y = 100;
+
         stage.addChild(helloLabel);
     }
+
     window.onload = Init; // Init is event handler
-})();
+
+})()
